1acbd72ab9b76a2314ced59e4f4d5413c97bd1d0
gcoayla <gcoayla.28@gmail.com>
2015-09-03 08:18:56 -0500

    arreglos.cpp

---
+++ arreglos.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void ordenar(int n[10]){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<9;j++){
+            if(n[j]>n[j+1]){
+                int temp=n[j+1];
+                n[j+1]=n[j];
+                n[j]=temp;
+            }
+        }
+    }
+}
+int main(){
+    int lista[10]{};
+    for(int i=0;i<10;i=i+1){
+        cout<<"Ingrese un numero"<<endl;
+        cin>>lista[i];
+    }
+    ordenar(lista);
+
+    for(int i=0;i<10;i++){
+        cout<<lista[i];
+        cout<<" ";
+    }
+
+    return 0;
+}
